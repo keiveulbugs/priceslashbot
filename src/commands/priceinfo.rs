837e35ecab1df6b25a1ebde63004e2e3536fcf62
kangalioo <jannik.a.schaper@web.de>
2022-09-28 18:31:45 +0200

    Fix formatting and warnings

--- src/commands/priceinfo.rs
+++ src/commands/priceinfo.rs
@@ -1,5 +1,3 @@
-use serde_json::json;
-
 use serde_derive::Deserialize;
 //use serde_derive::Serialize;
 use serde_json::Value;
@@ -70,42 +68,43 @@ pub async fn info_coin(
     #[description = "Enter a symbol, or choose a default one!"] address: String,
     #[description = "turn this on if you want to invert the pair"] invert: bool,
 ) -> Result<(), serenity::Error> {
-                let response = if let Ok(responses) = reqwest::get(format!(
-                    "https://api.dexscreener.com/latest/dex/pairs/{}/{}",
-                    chain, address
-                ))
-                .await
-                {
-                    let status = responses.status().to_string();
-                    if status == "200 OK" {
-                        if let Ok(v) = responses.json::<L1>().await {
-                            let w = v.pairs;
-                            if invert {
-                                let price0 = w[0].price_native.parse::<f64>().unwrap();
-                                let usd0 = w[0].price_usd.parse::<f64>().unwrap();
-                                let usd1 = usd0 / price0;
-                                let name1 = "token";
-                                let volume = &w[0].volume["h24"].to_string();
-                                format!("Price of {}:\n${}\nVolume: ${}", name1, usd1, volume)
-                            } else {
-                                let usd0 = w[0].price_usd.parse::<f64>().unwrap();
-                                let volume = &w[0].volume["h24"];
-                                let name0 = "token";
-                                let change0 = w[0].price_change.h24;
-                                format!(
-                                    "Price of {}:\n${}\nVolume: ${}\n24h Change {}%",
-                                    name0, usd0, volume, change0
-                                )
-                            }
-                        } else {
-                            "Something went wrong with parsing the data".to_string()
-                        }
-                    } else {
-                        "This pair can not be retrieved from dexscreener, make sure you write it down correctly".to_string()
-                    }
+    let response = if let Ok(responses) = reqwest::get(format!(
+        "https://api.dexscreener.com/latest/dex/pairs/{}/{}",
+        chain, address
+    ))
+    .await
+    {
+        let status = responses.status().to_string();
+        if status == "200 OK" {
+            if let Ok(v) = responses.json::<L1>().await {
+                let w = v.pairs;
+                if invert {
+                    let price0 = w[0].price_native.parse::<f64>().unwrap();
+                    let usd0 = w[0].price_usd.parse::<f64>().unwrap();
+                    let usd1 = usd0 / price0;
+                    let name1 = "token";
+                    let volume = &w[0].volume["h24"].to_string();
+                    format!("Price of {}:\n${}\nVolume: ${}", name1, usd1, volume)
                 } else {
-                    "The dexscreener api can not be reached".to_string()
-                };
+                    let usd0 = w[0].price_usd.parse::<f64>().unwrap();
+                    let volume = &w[0].volume["h24"];
+                    let name0 = "token";
+                    let change0 = w[0].price_change.h24;
+                    format!(
+                        "Price of {}:\n${}\nVolume: ${}\n24h Change {}%",
+                        name0, usd0, volume, change0
+                    )
+                }
+            } else {
+                "Something went wrong with parsing the data".to_string()
+            }
+        } else {
+            "This pair can not be retrieved from dexscreener, make sure you write it down correctly"
+                .to_string()
+        }
+    } else {
+        "The dexscreener api can not be reached".to_string()
+    };
 
     ctx.say(response).await?;
     Ok(())
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 mod commands;
 //Serenity crate for Discord
-use serenity::async_trait;
 use serenity::model::application::command::Command;
-use serenity::model::application::interaction::{Interaction, InteractionResponseType};
 use serenity::model::gateway::Ready;
 use serenity::model::id::GuildId;
 use serenity::model::prelude::ChannelId;
@@ -13,11 +11,11 @@ use serenity::prelude::*;
 extern crate dotenv_codegen;
 
 //Constants
-/// Your Bot token
+// Your Bot token
 const DISCORD_TOKEN: &str = dotenv!("DISCORD_TOKEN");
-/// If you want to have commands specific to only a specific guild, set this as your guild_id.
+// If you want to have commands specific to only a specific guild, set this as your guild_id.
 const PRIVATEGUILDID: GuildId = GuildId(703332075914264606);
-/// Error channel, instead of logging the terminal and booting into the server, it logs errors to a private Discord.
+// Error channel, instead of logging the terminal and booting into the server, it logs errors to a private Discord.
 const DISCORD_CHANNEL_ERROR: ChannelId = ChannelId(703332075914264609);
 
 async fn on_ready(
@@ -25,40 +23,40 @@ async fn on_ready(
     ready: &Ready,
     framework: &poise::Framework<(), serenity::Error>,
 ) -> Result<(), serenity::Error> {
-        /// To announce that the bot is online.
-        println!("{} is connected!", ready.user.name);
+    // To announce that the bot is online.
+    println!("{} is connected!", ready.user.name);
 
-        /// This registers commands for the bot, guild commands are instantly active on specified servers
-        ///
-        /// The commands you specify here only work in your own guild!
-        /// This is useful if you want to control your bot from within your personal server,
-        /// but dont want other servers to have access to it.
-        /// For example sending an announcement to all servers it is located in.
-        let builder = poise::builtins::create_application_commands(&framework.options().commands);
-        let commands = GuildId::set_application_commands(&PRIVATEGUILDID, &ctx.http, |commands| {
-            *commands = builder.clone();
-            commands
-        })
-        .await;
-        /// This line runs on start-up to tell you which commands succesfully booted.
-        println!(
-            "I now have the following guild slash commands: \n{:#?}",
-            commands
-        );
+    // This registers commands for the bot, guild commands are instantly active on specified servers
+    //
+    // The commands you specify here only work in your own guild!
+    // This is useful if you want to control your bot from within your personal server,
+    // but dont want other servers to have access to it.
+    // For example sending an announcement to all servers it is located in.
+    let builder = poise::builtins::create_application_commands(&framework.options().commands);
+    let commands = GuildId::set_application_commands(&PRIVATEGUILDID, &ctx.http, |commands| {
+        *commands = builder.clone();
+        commands
+    })
+    .await;
+    // This line runs on start-up to tell you which commands succesfully booted.
+    println!(
+        "I now have the following guild slash commands: \n{:#?}",
+        commands
+    );
 
-        /// Below we register Global commands, global commands can take some time to update on all servers the bot is active in
-        ///
-        /// Global commands are availabe in every server, including DM's.
-        /// We call the commands folder, the ping file and then the register function.
-        let global_command1 = Command::set_global_application_commands(&ctx.http, |commands| {
-            *commands = builder;
-            commands
-        })
-        .await;
-        println!(
-            "I now have the following guild slash commands: \n{:#?}",
-            global_command1
-        );
+    // Below we register Global commands, global commands can take some time to update on all servers the bot is active in
+    //
+    // Global commands are availabe in every server, including DM's.
+    // We call the commands folder, the ping file and then the register function.
+    let global_command1 = Command::set_global_application_commands(&ctx.http, |commands| {
+        *commands = builder;
+        commands
+    })
+    .await;
+    println!(
+        "I now have the following guild slash commands: \n{:#?}",
+        global_command1
+    );
 
     Ok(())
 }
@@ -80,8 +78,8 @@ async fn on_error(
 #[allow(unused_doc_comments)]
 #[tokio::main]
 async fn main() {
-    /// Build our client.
-    let mut client = poise::Framework::builder()
+    // Build our client.
+    let client = poise::Framework::builder()
         .token(DISCORD_TOKEN)
         .intents(GatewayIntents::empty())
         .options(poise::FrameworkOptions {
